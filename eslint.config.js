f3ad5ef9e6ad20f7b9eb0d0c50bb19b4b158f922
LGLabGreg <greg@lglab.co.uk>
2025-01-29 19:34:24 +0000

    chore: consistent-type-imports

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
     },
   },
 ];
