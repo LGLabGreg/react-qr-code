e06db8e7b1d2445b7b65bfa3710d5d7f951ab9a7
LGLabGreg <greg@lglab.co.uk>
2025-01-29 19:26:57 +0000

    chore: switch to eslint/prettier

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['**/dist', '**/qrcodegen'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
